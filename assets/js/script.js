fe6a4ed1706dff347ab54e510bff2688c0337b30
Hanjar1812000 <muhammethanjarn@gmail.com>
2022-01-09 18:55:58 +0300

    ITU project

---
+++ assets/js/script.js
@@ -0,0 +1,90 @@
+// Master DOManipulator v2 ------------------------------------------------------------
+const items = document.querySelectorAll('.item'),
+    controls = document.querySelectorAll('.control'),
+    headerItems = document.querySelectorAll('.item-header'),
+    descriptionItems = document.querySelectorAll('.item-description'),
+    activeDelay = .76,
+    interval = 5000;
+
+let current = 0;
+
+const slider = {
+    init: () => {
+        controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
+        controls[current].classList.add('active');
+        items[current].classList.add('active');
+    },
+    nextSlide: () => { // Increment current slide and add active class
+        slider.reset();
+        if (current === items.length - 1) current = -1; // Check if current slide is last in array
+        current++;
+        controls[current].classList.add('active');
+        items[current].classList.add('active');
+        slider.transitionDelay(headerItems);
+        slider.transitionDelay(descriptionItems);
+    },
+    clickedControl: (e) => { // Add active class to clicked control and corresponding slide
+        slider.reset();
+        clearInterval(intervalF);
+
+        const control = e.target,
+            dataIndex = Number(control.dataset.index);
+
+        control.classList.add('active');
+        items.forEach((item, index) => {
+            if (index === dataIndex) { // Add active class to corresponding slide
+                item.classList.add('active');
+            }
+        })
+        current = dataIndex; // Update current slide
+        slider.transitionDelay(headerItems);
+        slider.transitionDelay(descriptionItems);
+        intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+    },
+    reset: () => { // Remove active classes
+        items.forEach(item => item.classList.remove('active'));
+        controls.forEach(control => control.classList.remove('active'));
+    },
+    transitionDelay: (items) => { // Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
+        let seconds;
+
+        items.forEach(item => {
+            const children = item.childNodes; // .vertical-part(s)
+            let count = 1,
+                delay;
+
+            item.classList.value === 'item-header' ? seconds = .015 : seconds = .007;
+
+            children.forEach(child => { // iterate through .vertical-part(s) and style b element
+                if (child.classList) {
+                    item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
+                    child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
+                    count++;
+                }
+            })
+        })
+    },
+}
+
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init();
+
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    // If you don't know the name or want to use
+    // the webserver default set name = ''
+    link.setAttribute('download', name);
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
+
+function myFunction(event, li1, li2, actives, disable1, disable2) {
+    document.getElementById(li1).removeAttribute("class", "active");
+    document.getElementById(li2).removeAttribute("class", "active");
+    document.getElementById(event).setAttribute("class", "active");
+    document.getElementById(actives).style.display = 'block';
+    document.getElementById(disable1).style.display = 'none';
+    document.getElementById(disable2).style.display = 'none';
+}
